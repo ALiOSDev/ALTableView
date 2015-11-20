aa4fbab04025009147aeef4f6102c70ea3ec17ac
Abimael Barea <elabi3@gmail.com>
2015-11-20 23:18:06 +0100

    Actualizado Master

--- ALTableViewFrameworkExamples/ALTableViewFrameworkExamples/MasterViewController.m
+++ ALTableViewFrameworkExamples/ALTableViewFrameworkExamples/MasterViewController.m
@@ -19,18 +19,17 @@
 
 @implementation MasterViewController
 
+
+#pragma mark - View LifeCycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
-
-//    self.modeSectionsIndexTitles = YES;
-//    self.modeSectionsExpandable = YES;
-//    self.modeMoveCells = YES;
+    
     [self registerCells];
     [self reloadAllSections:[self createElements]];
     [self.tableView setTableFooterView:[[UIView alloc] init]];
-
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -44,45 +43,29 @@
 }
 
 
-#pragma mark - Segues
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = self.objects[indexPath.row];
-        DetailViewController *controller = (DetailViewController *)[[segue destinationViewController] topViewController];
-        [controller setDetailItem:object];
-        controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem;
-        controller.navigationItem.leftItemsSupplementBackButton = YES;
-    }
-}
-
-
-
+#pragma mark - Register Cells
 
 -(void) registerCells {
     [self registerClass:[MasterTableViewCell class] CellIdentifier:@"MasterTableViewCell"];
 }
 
 
-// Professional Example
+#pragma mark - Create Cells
 
 - (NSMutableArray *) createElements {
-    
     // Creamos las secciones
     NSMutableArray * sections = [NSMutableArray array];
+    
     NSMutableArray * rows = [NSMutableArray array];
     RowElement * example1 = [[RowElement alloc] initWithClassName:[MasterExample1TableViewCell class] object:@"Example 1" heightCell:@44 cellIdentifier:@"MasterTableViewCell"];
-    
-    RowElement * example2 = [[RowElement alloc] initWithClassName:[MasterExample2TableViewCell class] object:@"Example 2" heightCell:@44 cellIdentifier:@"MasterTableViewCell"];
-    
     [rows addObject:example1];
+
+    RowElement * example2 = [[RowElement alloc] initWithClassName:[MasterExample2TableViewCell class] object:@"Index Table View" heightCell:@44 cellIdentifier:@"MasterTableViewCell"];
     [rows addObject:example2];
     
     SectionElement * sectionElement = [[SectionElement alloc] initWithSectionTitleIndex:nil viewHeader:nil viewFooter:nil heightHeader:@0 heightFooter:@0 cellObjects:rows];
     
     [sections addObject:sectionElement];
-    
     return sections;
 }
 
