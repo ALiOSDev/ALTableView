762ca18ed59079c32fded65557b0c544bb9de6a5
lorenzo villarroel perez <villarroel.lorenzo@hotmail.com>
2018-04-20 11:02:37 +0200

    borrados los proyectos de objective-c

--- ALTableViewFrameworkExamples/ALTableViewFrameworkExamples/ALTableViewClasses/ALTableViewFramework-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "ALTableViewController.h"
-#import "RowElement.h"
-#import "SectionElement.h"
